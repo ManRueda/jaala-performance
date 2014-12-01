345b3942e77ddebd84810b90d368612c491250f2
Manuel Rueda <manuel.rueda.un@gmail.com>
2014-12-01 17:19:04 -0300

    README // change of directory // error check // better result highlight

---
+++ README.md
@@ -0,0 +1,8 @@
+jaala-performance
+=================
+
+Small performance comparative tool for Javascript.
+
+
+In my job and my life i have to try many options of pieces of code to test their performance, especially when a makes searches in an array or object.
+So i made this small tool to make my day more easy and i want to share it to make your days more easy too.
--- js/jaala-performance.js
+++ js/jaala-performance.js
@@ -15,6 +15,15 @@
     editors.push(this);
   }
 
+  function testError(func){
+    try{
+      func();
+      return undefined;
+    }catch(ex){
+      return ex;
+    }
+  }
+
 
 
   function Comparer(){
@@ -26,15 +35,20 @@
         code: arguments[i].code(),
         starts: undefined,
         ends: undefined,
-        time: undefined
+        time: undefined,
+        bestTime: undefined
       });
     }
 
     this.run = function run(){
+      this.bestTime = undefined;
       for(var i = 0; i < this.tests.length; i++){
         var func      = new Function([], this.tests[i].code);
         var timeStart = undefined;
         var timeEnd   = undefined;
+        var validate = testError(func);
+        if (validate)
+          return validate;
 
         timeStart = performance.now();
         func();
@@ -42,6 +56,10 @@
         this.tests[i].starts = timeStart;
         this.tests[i].ends = timeEnd;
         this.tests[i].time = this.tests[i].ends - this.tests[i].starts;
+        this.bestTime = 
+          this.bestTime > this.tests[i].time || this.bestTime == undefined 
+          ? this.tests[i].time 
+          : this.bestTime;
       }
     };
 
